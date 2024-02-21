af29879b91a6f0d313c876681cf68c331ee6c49d
Your Name <azzytm@yandex.ru>
2024-02-21 22:05:01 +0300

    Update

--- src/main/java/com/project/SpringTGBot/components/BotCommands.java
+++ src/main/java/com/project/SpringTGBot/components/BotCommands.java
@@ -1,4 +1,17 @@
 package com.project.SpringTGBot.components;
 
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+
+import java.util.List;
+
 public interface BotCommands {
+    List<BotCommand> LIST_OF_COMMANDS = List.of(
+        new BotCommand("/start", "Start bot"),
+            new BotCommand("/help", "bot info")
+    );
+
+    String HELP_TEXT = "This bot will help to count the number of messages in the chat. " +
+            "the following commands are available: \n" +
+            "/start - start bot \n" +
+            "/help - help menu";
 }
